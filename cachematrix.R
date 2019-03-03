cb05c26b7be64c859378c3ed07db0fabce97fa78
Madhu Milan <madhumilan37@gmail.com>
2019-03-03 19:32:05 +0530

    To modify the contents so as to suit to calculate the inverse of an invertible matrix, cache its data.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    my_inv <- NULL
+    set <- function(y) {
+      x <<- y
+      my_inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) {
+        message("Caching the data") 
+        my_inv <<- inverse
+    }
+    getinv <- function() my_inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## This function is to calculate the 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    my_inv <- x$getinv()
+    if(!is.null(my_inv)){
+      message("getting cached data")
+      return(my_inv)
+    }
+    data <- x$get()
+    my_inv <- solve(data, ...)
+    x$setinv(my_inv)
+    my_inv
 }
